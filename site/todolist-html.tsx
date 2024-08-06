b30724f4b4f00201cf8a5f348ef81bc68c3b0b31
sdegutis <sbdegutis@gmail.com>
2024-08-06 12:21:42 -0500

    Loading.

--- site/todolist-html.tsx
+++ site/todolist-html.tsx
@@ -1,6 +1,8 @@
-export function TodoListContainer(attrs: any, children: any) {
+export function TodoListContainer() {
   return <>
-    <div id='todo-root' />
+    <div id='todo-root'>
+      Loading...
+    </div>
     <script type='module' src='./todolist-client.js' />
   </>;
 }
