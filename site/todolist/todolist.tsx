bfe80b82a7837eb7efb313c35d44cfba84bbe7c7
sdegutis <sbdegutis@gmail.com>
2024-08-06 13:04:53 -0500

    Counter.

--- site/todolist/todolist.tsx
+++ site/todolist/todolist.tsx
@@ -15,21 +15,26 @@ export function TodoList() {
     }
   };
 
-  const numberDone = <span /> as HTMLSpanElement;
-  const updateNubmerDone = () => {
-    numberDone.textContent = `Done: ${list.doneItems().length}`;
-  };
-
-  updateNubmerDone();
-  list.onchange = updateNubmerDone;
-
   return <>
     <div>{input}</div>
     <div class='actions'>
-      {numberDone}
+      <Counter list={list} />
       <a href='#' onclick={(e: Event) => { e.preventDefault(); list.clearDone() }}>Clear done</a>
       <a href='#' onclick={(e: Event) => { e.preventDefault(); list.invertAll() }}>Invert all</a>
     </div>
     {list.ul}
   </>;
+}
+
+function Counter({ list }: { list: List }) {
+  const span = <span /> as HTMLSpanElement;
+
+  const updateText = () => {
+    span.textContent = `Done: ${list.doneItems().length}`;
+  };
+
+  updateText();
+  list.onchange = updateText;
+
+  return span;
 }
