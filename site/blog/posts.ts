33aa68bc8ff2bc1448d069cb49420189d79f8b7f
sdegutis <sbdegutis@gmail.com>
2024-08-06 14:59:43 -0500

    Comment.

--- site/blog/posts.ts
+++ site/blog/posts.ts
@@ -5,8 +5,8 @@ import postFiles from './data/';
 const md = new MarkdownIt();
 
 export const posts = postFiles.map(file => {
-  const rawContent = file.content.toString('utf8');
-  const lines = rawContent.split(/\r?\n/);
+  // Properly parsing .md frontmatter is out of scope of this demo
+  const lines = file.content.toString('utf8').split(/\r?\n/);
   const title = lines.shift()!;
   const content = md.render(lines.join('\n'));
   const slug = basename(file.path);
